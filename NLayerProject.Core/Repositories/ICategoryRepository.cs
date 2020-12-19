d8d9dc744f826270c144497e0431745a01e501f4
rabiaokumus <rabiaokumus@gmail.com>
2020-12-19 19:27:59 +0300

    created data layer repositories

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    interface ICategoryRepository : IRepository<Category>
+    {
+        Task<IEnumerable<Category>> GetWithProductsByIdAsync(int categoryId);
+    }
+}
